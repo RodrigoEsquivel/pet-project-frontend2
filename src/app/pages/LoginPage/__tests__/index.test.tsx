a8c6c1526e3bebf183ffccf982c68333d266b538
RodrigoEsquivel <luis.rodrigo.esquivel@gmail.com>
2022-03-07 14:11:47 -0600

    fixesTest

--- src/app/pages/LoginPage/__tests__/index.test.tsx
+++ src/app/pages/LoginPage/__tests__/index.test.tsx
@@ -4,8 +4,13 @@ import { render } from '@testing-library/react';
 import { LoginPage } from '..';
 
 describe('<LoginPage  />', () => {
+  const routeComponentProps = {
+    history: {} as any,
+    location: {} as any,
+    match: {} as any,
+  };
   it('should match snapshot', () => {
-    const loadingIndicator = render(<LoginPage />);
+    const loadingIndicator = render(<LoginPage {...routeComponentProps} />);
     expect(loadingIndicator.container.firstChild).toMatchSnapshot();
   });
 });
